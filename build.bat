926b34c4aaf13e78848783bcb9bc0e8994ab8c9a
Rudokhvist <kotlyar.andrey@gmail.com>
2024-03-18 13:24:14 +0200

    thank you dependabot, now fuck off

--- build.bat
+++ build.bat
@@ -21,7 +21,7 @@ if exist out rmdir /Q /S out
 
 rem release generic version
 
-dotnet publish -c "Release" -f "net6.0" -o "out/generic" "/p:LinkDuringPublish=false"
+dotnet publish -c "Release" -f "net7.0" -o "out/generic" "/p:LinkDuringPublish=false"
 mkdir .\out\%CurrDirName%
 copy .\out\generic\%CurrDirName%.dll .\out\%CurrDirName%
 7z a -tzip -mx7 .\out\%CurrDirName%.zip .\out\%CurrDirName%
@@ -30,7 +30,7 @@ rmdir /Q /S out\%CurrDirName%
 rem release generic-netf version
 rem comment section below if you don't target netf ASF version
 
-dotnet publish -c "Release" -f "net48" -o "out/generic-netf"
+dotnet publish -c "Release" -f "net481" -o "out/generic-netf"
 mkdir .\out\%CurrDirName%
 copy .\out\generic-netf\%CurrDirName%.dll .\out\%CurrDirName%
 7z a -tzip -mx7 .\out\%CurrDirName%-netf.zip .\out\%CurrDirName%
